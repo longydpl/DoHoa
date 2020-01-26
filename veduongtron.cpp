eb776ad1f7391839a7106320bb32380a76e42ae7
LongHoang <longydpl@gmail.com>
2020-01-26 23:51:52 +0700

    Update veduongtron.cpp

--- veduongtron.cpp
+++ veduongtron.cpp
@@ -11,18 +11,18 @@ void ve8diem(int x, int y ,int x0, int y0, int mau)
     putpixel(x0-y,x0+x,mau);
     putpixel(x0-y,x0-x,mau);
 }
-void vehinhtron(int x0, int y0, int r,int mau)
+void vehinhtron(int x0, int y0, int r)
 {
     int x = 0, y = r;
     int  p = 3 - 4*r;
     while(x<=y)
     {
-        ve8diem(x,y,x0,y0,mau);
+        ve8diem(x,y,x0,y0,RED);
         if(p<0) p += 4*x+6;
         else
         {
             p += 4*(x-y)+10;
-            y +=1;
+            y -=1;
         }
         x +=1;
     }
@@ -30,7 +30,7 @@ void vehinhtron(int x0, int y0, int r,int mau)
 int main()
 {
     initwindow(640,480);
-    vehinhtron(300,300,100,RED);
+    vehinhtron(300,300,100);
     getch();
     return 0;
 }
